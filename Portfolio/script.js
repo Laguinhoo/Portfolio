a607cd4f304a331ad9c4d489f98448d7f5454b27
Laguinhoo <143919764+Laguinhoo@users.noreply.github.com>
2024-10-02 21:24:40 -0300

    Add files via upload

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -4,7 +4,24 @@ window.addEventListener('scroll', onscroll)
 
 function onscroll() {
     console.log(window.scrollY)
-    if (window.scrollY >= 908) {
+
+    if (window.scrollY <= 900) {
+        header.style.backgroundColor = "#E1EBE2"
+        header.style.color = "#222423"
+    }
+
+    if (window.scrollY >= 853) {
         header.style.backgroundColor = "#8C80C2"
+        header.style.color = "#222423"
+    }
+
+    if (window.scrollY >= 1761) {
+        header.style.backgroundColor = "#605AA0"
+        header.style.color = "#222423"
+    }
+    
+    if (window.scrollY > 2600) {
+        header.style.backgroundColor = "#222423"
+        header.style.color = "#E1EBE2"
     }
 }
